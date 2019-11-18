4947b2b9d9197cb4864e5730b72500e590f241a0
gofreescout <mpbyrne@gmail.com>
2019-11-17 19:30:42 -0700

    some clean up

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -1,11 +1,9 @@
 module Types
   class QueryType < Types::BaseObject
-    # Add root-level fields here.
-    # They will be entry points for queries on your schema.
 
-    # TODO: remove me
     field :all_properties, [PropertyType], null: false,
       description: "An example field added by the generator"
+
     def all_properties
       Property.all
     end
